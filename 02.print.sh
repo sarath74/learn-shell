8166e73b96f57a1ff0a7c12a015b8de6eb3db693
sarath74 <125580270+sarath74@users.noreply.github.com>
2023-02-19 08:52:53 +0530

    color commands for printing

---
+++ 02.print.sh
@@ -0,0 +1,18 @@
+# color syntax
+# echo -e "\e[COLm MESSAGE \e[0m"
+# echo -e -> Enaable colors from echo command
+# \e[COLm -> enable a particular colors
+# color         codes
+# red             31
+# green           32
+# yellow          33
+# blue            34
+# magenta         35
+# cyan            36
+
+# \e[0m -> zero code is going to reset the color,meaning if we enable color for sure we want to disable it,
+ otherwise the  colors will continue  on screen
+ # echo -e "\e[31mHellow in Red color"
+ echo hello world
+
+
