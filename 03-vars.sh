5ffdcb162d915b06252bceea05af5014e05d25b4
sarath74 <125580270+sarath74@users.noreply.github.com>
2023-02-20 11:03:02 +0530

    input test

--- 03-vars.sh
+++ 03-vars.sh
@@ -6,7 +6,7 @@ DATE=$(date)
 echo "hello,Good Morning Today DATE is ${DATE}"
 
 # Arithmetic Substitution
-# ADD=$((2+3))
+ADD=$((2+3))
 echo "Addition of 2+3 = ${ADD}"
 
 ## Accessing Variable from Shell
