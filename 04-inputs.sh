c3ec5bfddd218b61ad89126479a124877f79f0cc
sarath74 <125580270+sarath74@users.noreply.github.com>
2023-02-20 10:32:27 +0530

    input test

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,3 +1,11 @@
-echo $1
-echo $2
+# 0-n ,$*/$@,$#
 
+echo $1 First Argument
+echo $2 Second Argument
+
+echo $0 #Script Name
+
+echo All Arguments = $*
+echo All Arguments = $@ # All Arguments
+
+echo Number of arguments = $# Number of Arguments
