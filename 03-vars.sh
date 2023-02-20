9a87a4b5f24e9e19ea8bca795ecf37c998e730f3
sarath74 <125580270+sarath74@users.noreply.github.com>
2023-02-20 10:15:56 +0530

    Variable commands testing

--- 03-vars.sh
+++ 03-vars.sh
@@ -4,3 +4,6 @@ echo "hello,Good Morning Today DATE is ${DATE}"
 # command substitution
 DATE=$(date)
 echo "hello,Good Morning Today DATE is ${DATE}"
+
+# Arithmetic SubstitutionADD=$((2+3))
+echo "Addition of 2+3 = ${ADD}"
